d30b9bf7f821fdd750196bec39b9f00def162b40
0187773933 <collincerbus@student.olympic.edu>
2025-03-04 12:26:43 -0500

    2

--- v1/routes/youtube.go
+++ v1/routes/youtube.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"fmt"
+	"bytes"
+	"strconv"
 	fiber "github.com/gofiber/fiber/v2"
 	server "github.com/0187773933/GO_SERVER/v1/server"
 	bolt "github.com/boltdb/bolt"
@@ -10,7 +12,7 @@ import (
 )
 
 
-func YouTube_ImportPlaylist( s *server.Server ) fiber.Handler {
+func YouTube_Playlist_Import( s *server.Server ) fiber.Handler {
 	return func( c *fiber.Ctx ) error {
 		playlist_id := c.Params( "playlist_id" )
 		playlist := youtube.GetVideosInPlaylist( s.Config.MiscMap[ "google_key" ] , playlist_id )
@@ -20,6 +22,8 @@ func YouTube_ImportPlaylist( s *server.Server ) fiber.Handler {
 			cl.AddNx( []byte( item.Snippet.ResourceId.VideoId ) )
 		}
 		s.DB.Update( func( tx *bolt.Tx ) error {
+			youtube_playlist_title , _ := tx.CreateBucketIfNotExists( []byte( "youtube-titles-playlist" ) )
+			youtube_playlist_title.Put( []byte( playlist_id ) , []byte( playlist.Name ) )
 			youtube_id_title , _ := tx.CreateBucketIfNotExists( []byte( "youtube-id-title" ) )
 			youtube_id_position , _ := tx.CreateBucketIfNotExists( []byte( "youtube-id-position" ) )
 			for _ , item := range playlist.Items {
@@ -32,6 +36,94 @@ func YouTube_ImportPlaylist( s *server.Server ) fiber.Handler {
 		})
 		return c.JSON( fiber.Map{
 			"result": true ,
+		})
+	}
+}
+
+func YouTube_Playlist_Get( s *server.Server ) fiber.Handler {
+	return func( c *fiber.Ctx ) error {
+		video_ids := []string{}
+		playlist_id := c.Params( "playlist_id" )
+		playlist_title := ""
+		s.DB.View( func( tx *bolt.Tx ) error {
+			cl_key := fmt.Sprintf( "youtube-playlist-%s" , playlist_id )
+			b := tx.Bucket( []byte( cl_key ) )
+			b.ForEach( func( k , v []byte ) error {
+				fmt.Printf( "key=%s , value=%s\n" , k , v )
+				video_ids = append( video_ids , string( v ) )
+				return nil
+			})
+			title_b := tx.Bucket( []byte( "youtube-titles-playlist" ) )
+			playlist_title = string( title_b.Get( []byte( playlist_id ) ) )
+			return nil
+		})
+		return c.JSON( fiber.Map{
+			"result": true ,
+			"playlist_id": playlist_id ,
+			"title": playlist_title ,
+			"ids": video_ids ,
+		})
+	}
+}
+
+func YouTube_Playlist_GetAll( s *server.Server ) fiber.Handler {
+	return func( c *fiber.Ctx ) error {
+		prefix := []byte( "youtube-playlist-" )
+		playlists := []string{}
+		s.DB.View( func( tx *bolt.Tx ) error {
+			tx.ForEach( func( bucket_name []byte , _ *bolt.Bucket ) error {
+				if bytes.HasPrefix( bucket_name , prefix ) == false { return nil }
+				id := string( bucket_name )[ len( prefix ) : ]
+				playlists = append( playlists , id )
+				fmt.Println( id )
+				return nil
+			})
+			return nil
+		})
+		return c.JSON( fiber.Map{
+			"result": true ,
+			"playlists": playlists ,
+		})
+	}
+}
+
+func YouTube_Playlist_Next( s *server.Server ) fiber.Handler {
+	return func( c *fiber.Ctx ) error {
+		playlist_id := c.Params( "playlist_id" )
+		playlist_title := ""
+		next_id := ""
+		next_title := ""
+		next_position := ""
+		s.DB.View( func( tx *bolt.Tx ) error {
+			playlist_title_b := tx.Bucket( []byte( "youtube-titles-playlist" ) )
+			position_b := tx.Bucket( []byte( "youtube-id-position" ) )
+			title_b := tx.Bucket( []byte( "youtube-id-title" ) )
+			playlist_title = string( playlist_title_b.Get( []byte( playlist_id ) ) )
+			cl_key := fmt.Sprintf( "youtube-playlist-%s" , playlist_id )
+			cl := circular.Open( s.DB , cl_key )
+			current , _ , _ := cl.Current()
+			current_id := string( current )
+			current_position := string( position_b.Get( current ) )
+			current_position_int , _ := strconv.Atoi( current_position )
+			if current_position_int == 0 {
+				next_id = current_id
+				next_title = string( title_b.Get( current ) )
+				next_position = "0"
+				return nil
+			}
+			next := cl.Next()
+			next_id = string( next )
+			next_title = string( title_b.Get( next ) )
+			next_position = "0"
+			return nil
+		})
+		return c.JSON( fiber.Map{
+			"result": true ,
+			"playlist_id": playlist_id ,
+			"playlist_title": playlist_title ,
+			"next_id": next_id ,
+			"next_title": next_title ,
+			"next_position": next_position ,
 		})
 	}
 }
