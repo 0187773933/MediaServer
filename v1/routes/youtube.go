7d569478a3cc37eae65e4bd9faebafbc73bd232b
0187773933 <collincerbus@student.olympic.edu>
2025-03-26 00:04:45 -0400

    10

--- v1/routes/youtube.go
+++ v1/routes/youtube.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	// "bytes"
 	// "strconv"
-	"strings"
+	// "strings"
+	"time"
 	"encoding/json"
 	fiber "github.com/gofiber/fiber/v2"
 	server "github.com/0187773933/GO_SERVER/v1/server"
@@ -18,6 +19,7 @@ type YoutubeVideo struct {
 	Id string `json:"id"`
 	Name string `json:"name"`
 	Position int `json:"position"`
+	Accessed int64 `json:"accessed"`
 	Tags []string `json:"tags"`
 }
 
@@ -25,35 +27,44 @@ type YoutubePlaylist struct {
 	Id string `json:"id"`
 	Name string `json:"name"`
 	Tags []string `json:"tags"`
+	Accessed int64 `json:"accessed"`
 	Index int `json:"index"`
 	Total int `json:"total"`
-	Videos []string `json:"videos"`
+	Videos []YoutubeVideo `json:"videos"`
+}
+
+type YoutubeSession struct {
+	Id string `json:"id"`
+	Tags []string `json:"tags"`
+	Accessed int64 `json:"accessed"`
+	Playlist YoutubePlaylist `json:"playlist"`
 }
 
 func YouTube_Playlist_Import( s *server.Server ) fiber.Handler {
 	return func( c *fiber.Ctx ) error {
 		playlist_id := c.Params( "playlist_id" )
 		playlist := youtube.GetVideosInPlaylist( s.Config.MiscMap[ "google_key" ] , playlist_id )
+		now := time.Now().UnixMilli()
 		var ytp YoutubePlaylist
 		ytp.Id = playlist_id
 		ytp.Name = playlist.Name
 		ytp.Index = 0
+		ytp.Accessed = now
 		ytp.Total = len( playlist.Items )
-		var videos []YoutubeVideo
 		for _ , item := range playlist.Items {
 			var v YoutubeVideo
 			v.Id = item.Snippet.ResourceId.VideoId
 			v.Name = item.Snippet.Title
 			v.Position = 0
-			videos = append( videos , v )
-			ytp.Videos = append( ytp.Videos , v.Id )
+			v.Accessed = now
+			ytp.Videos = append( ytp.Videos , v )
 		}
 		ytp_json , _ := json.Marshal( ytp )
 		s.DB.Update( func( tx *bolt.Tx ) error {
 			b , _ := tx.CreateBucketIfNotExists( []byte( "youtube-playlists" ) )
 			b.Put( []byte( playlist_id ) , ytp_json )
 			videos_b , _ := tx.CreateBucketIfNotExists( []byte( "youtube-videos" ) )
-			for _ , v := range videos {
+			for _ , v := range ytp.Videos {
 				v_json , _ := json.Marshal( v )
 				videos_b.Put( []byte( v.Id ) , v_json )
 			}
@@ -108,16 +119,31 @@ func YouTube_Playlist_Next_New_Session( s *server.Server ) fiber.Handler {
 		s.LOG.Debug( "YouTube_Playlist_Next_New_Session()" )
 		playlist_id := c.Params( "playlist_id" )
 		session_id := utils.GenULID()
-		route_info := c.Route()
-		prefix_parts := strings.Split( route_info.Path , "/:playlist_id" )
-		prefix := prefix_parts[ 0 ]
-		url := fmt.Sprintf( "%s/%s/%s/next" , prefix , playlist_id , session_id )
-		s.LOG.Debug( url )
-		return c.Redirect( url )
-		// return c.RedirectToRoute( "youtube_playlist_session_next", fiber.Map{
-		// 	"name":
-		// 	"params": map[string]string{ "playlist_id": playlist_id , "session_id": session_id } ,
-		// })
+		// route_info := c.Route()
+		// prefix_parts := strings.Split( route_info.Path , "/playlist/:playlist_id" )
+		// prefix := prefix_parts[ 0 ]
+		// url := fmt.Sprintf( "%s/%s/next" , prefix , session_id )
+		// s.LOG.Debug( url )
+		now := time.Now().UnixMilli()
+		s.DB.Update( func( tx *bolt.Tx ) error {
+			sb , _ := tx.CreateBucketIfNotExists( []byte( "youtube-sessions" ) )
+			var session YoutubeSession
+			session.Id = session_id
+			session.Playlist.Id = playlist_id
+			session.Accessed = now
+			playlist_b := tx.Bucket( []byte( "youtube-playlists" ) )
+			playlist_json := playlist_b.Get( []byte( playlist_id ) )
+			json.Unmarshal( playlist_json , &session.Playlist )
+			s.LOG.Debug( session )
+			session_json , _ := json.Marshal( session )
+			sb.Put( []byte( session_id ) , []byte( session_json ) )
+			return nil
+		})
+
+		// return c.Redirect( url )
+		return c.RedirectToRoute( "youtube_session", fiber.Map{
+			"session_id": session_id ,
+		})
 
 		// fiber v3 only
 		// return c.Redirect().Route( "youtube_playlist_session_next" , fiber.RedirectConfig{
@@ -126,63 +152,60 @@ func YouTube_Playlist_Next_New_Session( s *server.Server ) fiber.Handler {
 		// 		"session_id":  session_id ,
 		// 	} ,
 		// })
-
-
-		// url := fiber.RouteURL( "youtube_playlist_session_next" , fiber.Map{
-		// 	"playlist_id": playlist_id ,
-		// 	"session_id":  session_id ,
-		// })
 	}
 }
 
-func YouTube_Playlist_Next( s *server.Server ) fiber.Handler {
+func YouTube_Session_Next( s *server.Server ) fiber.Handler {
 	return func( c *fiber.Ctx ) error {
 		s.LOG.Debug( "YouTube_Playlist_Next()" )
-		playlist_id := c.Params( "playlist_id" )
 		session_id := c.Params( "session_id" )
+		now := time.Now().UnixMilli()
 		// var ytp YoutubePlaylist
-		// var next_id string
-		// var next_index int
-		// var next_position int
-		// s.DB.Update( func( tx *bolt.Tx ) error {
-		// 	b := tx.Bucket( []byte( "youtube-playlists" ) )
-		// 	ytp_json := b.Get( []byte( playlist_id ) )
-		// 	json.Unmarshal( ytp_json , &ytp )
-		// 	if ytp.Videos[ ytp.Index ].Position == -1 {
-		// 		ytp.Index++
-		// 		if ytp.Index >= ytp.Total {
-		// 			ytp.Index = 0
-		// 		}
-		// 		if ytp.Videos[ ytp.Index ].Position == -1 {
-		// 			next_index = ytp.Index
-		// 			next_position = 0
-		// 		}
-		// 		next_id = ytp.Videos[ ytp.Index ].Id
-		// 		next_index = ytp.Index
-		// 		next_position = ytp.Videos[ ytp.Index ].Position
-		// 	} else {
-		// 		// continuing where you left off , not finished
-		// 		next_id = ytp.Videos[ ytp.Index ].Id
-		// 		next_index = ytp.Index
-		// 		next_position = ytp.Videos[ ytp.Index ].Position
-		// 		fmt.Println( "continuing where you left off" , next_id , next_index , next_position )
-		// 	}
-		// 	// restore
-		// 	ytp_json , _ = json.Marshal( ytp )
-		// 	b.Put( []byte( playlist_id ) , ytp_json )
-		// 	return nil
-		// })
-		// return c.JSON(fiber.Map{
-		// 	"result": true ,
-		// 	"playlist_id": playlist_id ,
-		// 	"next_id": next_id ,
-		// 	"next_index": next_index ,
-		// 	"next_position": next_position ,
-		// })
+		var next_id string
+		var next_index int
+		var next_position int
+		var session YoutubeSession
+		s.DB.Update( func( tx *bolt.Tx ) error {
+			b := tx.Bucket( []byte( "youtube-sessions" ) )
+			session_json := b.Get( []byte( session_id ) )
+			if session_json == nil {
+				s.LOG.Fatal( "session json is empty ??" )
+				return nil
+			}
+			json.Unmarshal( session_json , &session )
+			fmt.Println( session )
+			session.Accessed = now
+			if session.Playlist.Videos[ session.Playlist.Index ].Position == -1 {
+				session.Playlist.Index++
+				if session.Playlist.Index >= session.Playlist.Total { // look for off by 1
+					session.Playlist.Index = 0
+				}
+				if session.Playlist.Videos[ session.Playlist.Index ].Position == -1 {
+					next_index = session.Playlist.Index
+					next_position = 0
+				}
+				next_id = session.Playlist.Videos[ session.Playlist.Index ].Id
+				next_index = session.Playlist.Index
+				next_position = session.Playlist.Videos[ session.Playlist.Index ].Position
+			} else {
+				// continuing where you left off , not finished
+				next_id = session.Playlist.Videos[ session.Playlist.Index ].Id
+				next_index = session.Playlist.Index
+				next_position = session.Playlist.Videos[ session.Playlist.Index ].Position
+				s.LOG.Debug( fmt.Sprintf( "continuing where you left off : %s : %d : %d" , next_id , next_index , next_position ) )
+			}
+			// restore
+			session_json , _ = json.Marshal( session )
+			b.Put( []byte( session_id ) , session_json )
+			return nil
+		})
 		return c.JSON(fiber.Map{
 			"result": true ,
-			"playlist_id": playlist_id ,
 			"session_id": session_id ,
+			"playlist_id": session.Playlist.Id ,
+			"next_id": next_id ,
+			"next_index": next_index ,
+			"next_position": next_position ,
 		})
 	}
 }
